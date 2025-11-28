b1a99bec2b93c9181adc05bfad13f914e4944702
IsaacLira42 <isaaclira422@gmail.com>
2025-11-28 15:57:30 -0300

    refactor(services/): Adiciona o metodo 'toResponse' que restrige quais dados que serao enviados pela API

--- src/services/Cart.service.ts
+++ src/services/Cart.service.ts
@@ -1,5 +1,6 @@
 import { CartRepository } from "@/repository/Cart.repository";
 import { CreateCartDto, ResponseCartDto } from "@/types/cart/cart.dto";
+import { Cart } from "@prisma/client";
 
 export class CartService {
 	private cartRespository: CartRepository;
@@ -8,15 +9,32 @@ export class CartService {
 		this.cartRespository = new CartRepository();
 	}
 
-	findById = async (id: number) => {
-		return await this.cartRespository.findById(id);
+	findById = async (id: number): Promise<ResponseCartDto | null> => {
+		const cart = await this.cartRespository.findById(id);
+		if (!cart) {
+			return null;
+		}
+		return this.toResponse(cart);
 	};
 
 	create = async (data: CreateCartDto): Promise<ResponseCartDto> => {
-		return await this.cartRespository.create(data);
+		const cart = await this.cartRespository.create(data);
+		return this.toResponse(cart);
 	};
 
 	delete = async (id: number): Promise<ResponseCartDto | null> => {
-		return await this.cartRespository.delete(id);
+		const cart = await this.cartRespository.delete(id);
+		if (!cart) {
+			return null;
+		}
+		return this.toResponse(cart);
+	};
+
+	private toResponse = (cart: Cart): ResponseCartDto => {
+		return {
+			id: cart.id,
+			subtotal: cart.subtotal,
+			total: cart.total,
+		};
 	};
 }
--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -3,6 +3,7 @@ import {
 	CreateProductDto,
 	ResponseProductDto,
 } from "@/types/product/product.dto";
+import { Product } from "@prisma/client";
 
 export class ProductService {
 	private productRepository: ProductRepository;
@@ -11,15 +12,30 @@ export class ProductService {
 		this.productRepository = new ProductRepository();
 	}
 
-	findAll = async () => {
-		return await this.productRepository.findAll();
+	findAll = async (): Promise<ResponseProductDto[]> => {
+		const products = await this.productRepository.findAll();
+		return products.map(this.toResponse);
 	};
 
-	findById = async (id: number) => {
-		return await this.productRepository.findById(id);
+	findById = async (id: number): Promise<ResponseProductDto | null> => {
+		const product = await this.productRepository.findById(id);
+		if (!product) {
+			return null;
+		}
+		return this.toResponse(product);
 	};
 
 	create = async (data: CreateProductDto): Promise<ResponseProductDto> => {
-		return await this.productRepository.create(data);
+		const product = await this.productRepository.create(data);
+		return this.toResponse(product);
+	};
+
+	private toResponse = (product: Product): ResponseProductDto => {
+		return {
+			id: product.id,
+			name: product.name,
+			price: product.price,
+			imageUrl: product.imageUrl,
+		};
 	};
 }
